9d786328bd1b1dabb3c1cdd5a894647c02b274c9
Wendell-maker <talesofsky@gmail.com>
2025-12-22 11:26:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 from fugle_marketdata import RestClient
-from google import genai
+import google.generativeai as genai
 from datetime import datetime
 import pytz
 from streamlit_autorefresh import st_autorefresh
